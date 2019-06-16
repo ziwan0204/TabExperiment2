73c084783dff40bc599753439d95a735c03bc0bc
Zi Wan <yipzw-wm17@student.tarc.edu.my>
2019-06-17 00:38:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tabexperiment/PagerAdapter.java
@@ -0,0 +1,26 @@
+package com.example.tabexperiment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+        this.mNumOfTabs = mNumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new TabFragment1();
+            case 1: return new TabFragment2();
+            case 2: return new TabFragment3();
+            default: return null;
+    }
+
+    @Override
+    public int getCount() { return mNumOfTabs;}
+}
